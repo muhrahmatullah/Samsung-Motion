ab1ec38b62380446a598e8271fc852a3b6b28dff
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2017-12-06 22:08:32 +0800

    initial commit

---
+++ app/src/main/java/com/example/muhrahmatullah/motioncool/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.muhrahmatullah.motioncool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
